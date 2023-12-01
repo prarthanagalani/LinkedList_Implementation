1181fdf96fa262c3a486cc98e50469c014e0253f
prarthanagalani <prarthanagalani@gmail.com>
2023-12-01 17:44:04 +0530

    added: Uc8: refactored Test class-day9

--- LinkedListTest.java
+++ LinkedListTest.java
@@ -21,4 +21,26 @@ public class LinkedListTest {
         assertNotNull("Node with value 30 should be found", searchedNode);
         assertEquals("Node should have data 30", 30, searchedNode.data);
     }
+
+    @Test
+    public void testInsertion()
+    {
+        // Create an instance of the class under test
+        LLImplementation LL = new LLImplementation();
+
+        // Add nodes to the linked list
+        LL.addNode(70);
+        LL.addNode(56);
+        LL.addNode(30);
+
+        //search a node with value 30
+        Node searchedNode = LL.search(30);
+
+        //perform assertions
+        assertNotNull("Node with value 30 should be found",searchedNode);
+
+        //Insert 40 after node with value 30
+        LL.InsertNodeAfterGivenNode(30,40);
+        
+    }
 }
